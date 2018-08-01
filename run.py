719157c518fda46f0edb7ad50105263971419f4b
blackruby <zapper0703@gmail.com>
2018-08-01 17:30:00 +0900

    set env for deploy

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from flask_migrate import Migrate
 from app import create_app, db
 from app.models import *
 
-app = create_app()
+application = create_app()
+app = application
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
